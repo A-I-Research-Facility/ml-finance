5af6d546aaab1260c341d732f590498899ba0d05
Nimesh Srivastava <snimesh412@gmail.com>
2024-11-18 01:42:34 +0530

    Single pattern finder

--- algo-tr.py
+++ algo-tr.py
@@ -1,22 +1,49 @@
+from functools import reduce
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import matplotlib.dates as mdates
 import numpy as np
+import time
 
+date, bid, ask = np.loadtxt('GBPUSD1d.txt', unpack=True, delimiter=',', converters={0: lambda x: mdates.datestr2num(x)})
 
 def percentChange(startPoint, currentPoint):
-    return ((currentPoint - startPoint)/startPoint) * 100
+    return ((float(currentPoint) - startPoint)/startPoint) * 100.00
 
 
+def patternFinder():
+    avgLine = ((bid + ask) / 2)
+    x = len(avgLine) - 30
+
+    y = 11
+    while y < x:
+        p1 = percentChange(avgLine[y - 10], avgLine[y - 9])
+        p2 = percentChange(avgLine[y - 10], avgLine[y - 8])
+        p3 = percentChange(avgLine[y - 10], avgLine[y - 7])
+        p4 = percentChange(avgLine[y - 10], avgLine[y - 6])
+        p5 = percentChange(avgLine[y - 10], avgLine[y - 5])
+        p6 = percentChange(avgLine[y - 10], avgLine[y - 4])
+        p7 = percentChange(avgLine[y - 10], avgLine[y - 3])
+        p8 = percentChange(avgLine[y - 10], avgLine[y - 2])
+        p9 = percentChange(avgLine[y - 10], avgLine[y - 1])
+        p10 = percentChange(avgLine[y - 10], avgLine[y])
+
+        outcomeRange = avgLine[y + 20 : y + 30]
+        currentPoint = avgLine[y]
+
+        print (reduce(lambda x, y : x+y, outcomeRange) / len(outcomeRange))
+        print (currentPoint)
+        print ('______')
+        print (p1, p2, p3, p4, p5, p6, p7, p8, p9, p10)
+        y += 1
+        time.sleep(5000)
 
 
 
 def graphRawFX():
-    date, bid, ask = np.loadtxt('GBPUSD1d.txt', unpack=True, delimiter=',', converters={0: lambda x: mdates.datestr2num(x)})
-
-    fig = plt.figure(figsize=(10, 7))
-    ax1 = plt.subplot2grid((40, 40), (0,0), rowspan=40, colspan=40)
+    fig = plt.figure(figsize = (10, 7))
+    ax1 = plt.subplot2grid((40, 40), (0, 0), rowspan = 40, colspan = 40)
 
     ax1.plot(date, bid)
     ax1.plot(date, ask)
@@ -36,3 +63,4 @@ def graphRawFX():
     plt.show()
 
 # graphRawFX()
+patternFinder()
